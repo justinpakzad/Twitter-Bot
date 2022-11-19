1cd7ba741c72891557b4200857b459831aeab9d7
quantium <alexcollsoutumuro@gmail.com>
2022-11-19 10:37:51 +0100

    add log_in

--- main.py
+++ main.py
@@ -8,3 +8,24 @@ from time import sleep
 import keypair
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+def log_in ( user=keypair.user, pasw=keypair.pasw ):
+  driver.get('https://www.twitter.com/i/flow/login')
+  sleep(2)
+  user_in = driver.find_element(by=By.TAG_NAME, value='input')
+  user_in.send_keys(user)
+  user_in.send_keys(Keys.ENTER)
+  sleep(2)
+  pasw_in = driver.find_element(by=By.NAME, value='password')
+  pasw_in.send_keys(pasw)
+  pasw_in.send_keys(Keys.ENTER)
+
+
+
+log_in()
+
+driver.refresh()
+
+tweets = driver.find_elements(by=By.TAG_NAME, value='article')
+sleep(2)
